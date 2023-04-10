a1a2ecef5e9350c44f5950016668bc6ddb51625e
skullsyed <syedjack07@gmail.com>
2023-04-10 06:52:00 +0530

    create cart using map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Cart from './components/cart';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Cart from "./components/cart";
 
 function App() {
+  const productName = ["Iphone 13", "Onplus 9", "Redmi 13"];
+  const price = ["$350", "$250", "$100"];
+
   return (
     <div className="App">
-  <Header/>
-<Cart/>
-<Footer/>
+      <Header />
+      {productName.map((nm) => (
+        <Cart name={nm} />
+      ))}
+
+      <Footer />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,38 +1,62 @@
 import React from "react";
 // import { useState } from "react";
 // import './bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './header.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./header.css";
 
-const Header = ()=>{
-// const [cartItem, setCartIteam] = useState();
+const Header = () => {
+  // const [cartItem, setCartIteam] = useState();
 
-return(
-  <div>
-    <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom  header-potion">
-    <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-      <li><a href="#" className="nav-link px-2 link-secondary">Home</a></li>
-      <li><a href="#" className="nav-link px-2 link-dark">Features</a></li>
-      <li><a href="#" className="nav-link px-2 link-dark">Pricing</a></li>
-      <li><a href="#" className="nav-link px-2 link-dark">FAQs</a></li>
-      <li><a href="#" className="nav-link px-2 link-dark">About</a></li>
-    </ul>
+  return (
+    <div>
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom  header-potion">
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <a href="#" className="nav-link px-2 link-secondary">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2 link-dark">
+              Features
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2 link-dark">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2 link-dark">
+              FAQs
+            </a>
+          </li>
+          <li>
+            <a href="#" className="nav-link px-2 link-dark">
+              About
+            </a>
+          </li>
+        </ul>
 
-    <div className="col-md-3 text-end">
-      <button type="button" className="btn btn-outline-primary me-2 login-button">Login</button>
-      <button type="button" className="btn btn-primary">Sign-up</button>
-      <button class="btn btn-outline-dark" type="submit">
-                            <i class="bi-cart-fill me-1"></i>
-                            Cart
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
-                        </button>
+        <div className="col-md-3 text-end">
+          <button
+            type="button"
+            className="btn btn-outline-primary me-2 login-button"
+          >
+            Login
+          </button>
+          <button type="button" className="btn btn-primary">
+            Sign-up
+          </button>
+          <button class="btn btn-outline-dark" type="submit">
+            <i class="bi-cart-fill me-1"></i>
+            Cart
+            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+          </button>
+        </div>
+      </header>
     </div>
-  </header>
-  </div>
-
-)
-
-
-}
+  );
+};
 
 export default Header;
