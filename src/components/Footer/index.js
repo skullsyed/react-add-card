059a7cfa5ca24f811606234b6250f1fa13e004b6
skullsyed <syedjack07@gmail.com>
2023-04-10 05:34:32 +0530

    add to cart with react

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import './footer.css';
+
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="py-5 bg-dark footer-line">
+        <div className="container footer-container">
+          <p className="m-0 text-center text-white">
+            Copyright © Your Website 2023
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/cart/index.js
@@ -0,0 +1,366 @@
+import "./cart.css";
+
+const Cart = () => {
+  return (
+    <div className="cart-base">
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-5">
+        <div class="card h-100 cart-container">
+          <div class="card-body p-4">
+            <div class="text-center">
+              <h5 class="fw-bolder">Fancy Product</h5>
+              $40.00 - $80.00
+            </div>
+          </div>
+          <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center">
+              <a class="btn btn-outline-dark mt-auto" href="#">
+                View options
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
